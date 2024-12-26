acd338f28d9fe3becd5d216b2b6dfea8b87f1987
Sadre_321 <haydaremre31@gmail.com>
2024-12-26 20:57:35 +0300

    adding Layout design

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Layout from './Layout/Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Layout>
     <App />
-  </StrictMode>,
+  </Layout>
 )
